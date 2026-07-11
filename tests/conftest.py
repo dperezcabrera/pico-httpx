19702d2e029d7618c78f0ac2784bde2befb7ff92
dperezcabrera <dperezcabrera@gmail.com>
2026-07-11 09:05:33 +0200

    Adopt pico-testing 0.2.0 pico_module and drop duplicated conftest fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,28 +1,7 @@
 import httpx
 import pytest
-from pico_ioc import DictSource, configuration, init
 
 from pico_httpx import client as client_module
-
-
-@pytest.fixture(autouse=True)
-def isolate_from_installed_plugins(monkeypatch):
-    monkeypatch.setenv("PICO_BOOT_AUTO_PLUGINS", "false")
-
-
-@pytest.fixture
-def make_container():
-    created = []
-
-    def _make(*modules, config=None):
-        cfg = configuration(DictSource(config or {}))
-        container = init(modules=["pico_httpx", *modules], config=cfg)
-        created.append(container)
-        return container
-
-    yield _make
-    for c in reversed(created):
-        c.shutdown()
 
 
 @pytest.fixture
